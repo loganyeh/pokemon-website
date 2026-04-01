c21d370ec243cf40f0ff217fbcf97cd0562802a5
loganyeh <yehlogan@gmail.com>
2026-03-31 23:19:16 -0400

    basic layout to begin. tailwind, boxicons, folders

---
+++ src/Pages/Homepage.tsx
@@ -0,0 +1,13 @@
+
+
+function Homepage(){
+
+    return(
+        <>
+            <div className="border border-red-600 h-screen w-screen"></div>
+        
+        </>
+    )
+}
+
+export default Homepage;
